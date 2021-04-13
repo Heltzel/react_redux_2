92856daf483c8f83708ea1f9ade78db79cc90563
Heltzel <heltzel.service@hotmail.com>
2021-04-13 11:51:41 +0200

    React app  Hooks: useSelector,  useDispatch

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -3,10 +3,15 @@ import CakeContainer from './components/CakeContainer'
 import store from './redux/Store'
 
 import { Provider } from 'react-redux'
+import HooksCakeContainer from './components/HooksCakeContainer'
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <h6>Hook component</h6>
+        <HooksCakeContainer />
+        <hr />
+        <h6>Standard component</h6>
         <CakeContainer />
       </div>
     </Provider>
---
+++ react-redux-demo/src/components/HooksCakeContainer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+// import action creator
+import { buyCake } from '../redux'
+
+// Read in the React-Redux docs about usage warnings of hooks
+
+function HooksCakeContainer() {
+  // Hook useSelector equevalent to mapStateToProps()
+  const numOfCakes = useSelector((state) => state.numOfCakes)
+  // Hook useDispatch returns a refference to the dispatch function from the Redux store
+  // Make sure to import the action creator
+  const dispatch = useDispatch()
+  return (
+    <div>
+      <h1>Number of cakes - {numOfCakes} </h1>
+      <button
+        onClick={() => {
+          dispatch(buyCake())
+        }}
+      >
+        Buy cake
+      </button>
+    </div>
+  )
+}
+
+export default HooksCakeContainer
