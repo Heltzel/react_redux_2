bebab00f0a21df84b46686934f0510df9891e832
Heltzel <heltzel.service@hotmail.com>
2021-04-13 14:38:23 +0200

    react app Ownprops

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -6,11 +6,15 @@ import { Provider } from 'react-redux'
 import HooksCakeContainer from './components/HooksCakeContainer'
 import IcecreamContainer from './components/IcecreamContainer'
 import NewCakeContainer from './components/NewCakeContainer'
+import ItemContainer from './components/ItemContainer'
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
+        <hr />
         <h6>Hook component</h6>
         <HooksCakeContainer />
         <hr />
