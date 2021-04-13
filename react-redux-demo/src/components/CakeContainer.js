848d0a2c2f7334c7071fa6051be206ee1ea54a3e
Heltzel <heltzel.service@hotmail.com>
2021-04-13 11:00:12 +0200

    react app  Connect comp to Redux store and actions

--- react-redux-demo/src/components/CakeContainer.js
+++ react-redux-demo/src/components/CakeContainer.js
@@ -1,12 +1,43 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { buyCake } from '../redux'
 
-function CakeContainer() {
+// connect redux to const app
+// 1 - define mapStateToProps
+// 2 - define mapDispatchToProps
+// 3 - connect higher order component from react-redux
+
+function CakeContainer(props) {
   return (
     <div>
-      <h1>Number of cakes</h1>
-      <button>Buy cake</button>
+      <h1>Number of cakes - {props.numOfCakes}</h1>
+      <button onClick={props.buyCake}>Buy cake</button>
     </div>
   )
 }
+// read in docs about 'selectors'
 
-export default CakeContainer
+/**
+ * @param state
+ * @return Obj
+ * purpose:  To access the Redux state in the component
+ */
+const mapStateToProps = (state) => {
+  return {
+    numOfCakes: state.numOfCakes,
+  }
+}
+
+/**
+ * @param dispatch
+ * @ return Obj
+ * purpose: Maps action creators to props
+ */
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  }
+}
+
+// Connects the component to the Redux store
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer)
